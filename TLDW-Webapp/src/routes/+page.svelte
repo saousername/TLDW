f3a5514e2f29255896235f3bd07ddb08333b7a1c
angolcrypto <angolcrypto@gmail.com>
2023-07-25 01:00:44 +0100

    -

---
+++ TLDW-Webapp/src/routes/+page.svelte
@@ -0,0 +1,95 @@
+<script>
+	import MarkdownIt from 'markdown-it';
+	import sanitizeHtml from 'sanitize-html';
+	import { afterUpdate } from 'svelte';
+	import { test_spit } from '../test_spit';
+
+	const md = new MarkdownIt();
+
+	let videoUrl = '';
+	const testing = false;
+	let spit = !testing ? 'Not generated a summary yet...' : formatSpit(test_spit);
+	let loading = false;
+	let pre_spit = '';
+
+	async function generateSummary() {
+		loading = true;
+		const responseObject = await fetch(`/api/generate?videoUrl=${videoUrl}`);
+
+		if (responseObject.status !== 200) {
+			spit = 'error: api call failed!';
+			loading = false;
+			return;
+		}
+
+		const responseJson = await responseObject.json();
+		const responseSpit = responseJson.spit;
+
+		console.log(responseSpit);
+		spit = formatSpit(responseSpit);
+		loading = false;
+	}
+
+	function formatSpit(toFormat) {
+		return sanitizeHtml(md.render(toFormat), {
+			allowedTags: sanitizeHtml.defaults.allowedTags.concat(['h1', 'h2', 'img'])
+		});
+	}
+</script>
+
+<div class="px-5 py-7 md:p-8 lg:p-14 xl:p-20 flex flex-col items-center justify-center">
+	<div class="flex flex-col items-center justify-center space-y-10">
+		<div class="flex flex-col items-center justify-center max-w-[400px] space-y-3">
+			<div class="flex flex-col items-center justify-center space-y-1">
+				<h1 class="text-center font-semibold text-4xl text-blue-600 tracking-tighter">TLDW</h1>
+				<h2 class="text-center font-light text-xl text-blue-500 tracking-tighter">
+					Too Long, Don't Watch
+				</h2>
+			</div>
+			<p class="text-center">
+				Enter a video URL and we'll give you the gist of what you'd learn from the video.
+			</p>
+		</div>
+
+		<div class="flex flex-col items-center justify-center space-y-3">
+			<input
+				class="bg-gray-100 p-3 w-full"
+				bind:value={videoUrl}
+				name="videoUrl"
+				type="text"
+				placeholder="YouTube Video URL"
+				disabled={loading}
+			/>
+			<button
+				class="bg-blue-600 px-10 py-3 w-full text-white"
+				disabled={loading}
+				on:click={generateSummary}
+			>
+				Generate Summary
+			</button>
+		</div>
+	</div>
+
+	<div class="w-full bg-black h-[1px] bg-opacity-20 my-10 lg:my-16" />
+
+	{#if loading}
+		<div class="flex flex-col items-center justify-center max-w-[400px] space-y-1">
+			<p class="2xl:max-w-[70vw] text-center text-lg text-blue-600 tracking-tight">LOADING</p>
+			<p class="2xl:max-w-[70vw] text-center font-light text-blue-400 tracking-tight">
+				This shouldn't take more than a minute or so though.
+			</p>
+			<img
+				class="w-full"
+				alt="Funny loading GIF of child sat on chair bored while waiting for page to load"
+				src="https://media.tenor.com/Y7ShQ_3hnn8AAAAd/me-waiting-for-my-friends-to-get-online.gif"
+			/>
+		</div>
+	{:else}
+		<!-- <p class="2xl:max-w-[70vw] text-left text-lg leading-9 tracking-wide" id="response">
+            {spit}
+        </p> -->
+		<div class="2xl:max-w-[70vw] prose-sm md:prose-md lg:prose-lg prose-li:list-disc">
+			{@html spit}
+		</div>
+	{/if}
+</div>
---
+++ TLDW-Webapp/src/routes/api/generate/+server.ts
@@ -0,0 +1,88 @@
+import { json } from "@sveltejs/kit";
+import type { RequestEvent } from "./$types";
+
+import { Configuration, OpenAIApi } from "openai";
+
+// 1. Get YouTube video transcript from YouTube's API
+// 2. Give transcript to ChatGPT API with a prompt "Summarize this video for me given the above transcript"
+// 3. Return answer to the user
+// 4. Profit
+
+// TODO: SWAP TO ENVIRONMENT VARIABLE WITH PROCESS.ENV OR SVELTE ALTERNATIVE AND CREATE .ENV FILE (DON'T FORGOT TO DEFINE .ENV VARIABLE ON VERCEL)
+const OPENAI_API_KEY = "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";
+
+async function getVideoCaptions(videoUrl: string): Promise<string | null> {
+    try {
+        const videoId = extractVideoIdFromUrl(videoUrl);
+        if (!videoId) {
+            throw new Error("Unable to extract Video ID from URL.");
+        }
+        const transcriptionCall = await fetch(`https://accurateadmiredfirmware.saousername.repl.co/api/get_captions?video_id=${videoId}`)
+
+        const transcription = transcriptionCall.text();
+
+        return transcription;
+    } catch (error: any) {
+        console.error('Error fetching captions:', error.message);
+        return null;
+    }
+}
+
+// Helper function to extract video ID from the URL
+function extractVideoIdFromUrl(url: string): string | null {
+    const videoIdRegex = /(?:v=|\/embed\/|\/watch\?v=|\/\w+\/\w+\/|\/v\/)([^&?\n]+)/;
+    const match = url.match(videoIdRegex);
+    return match && match[1];
+}
+
+export async function GET({ request, params, url }: RequestEvent) {
+    const searchParams = url.searchParams;
+    const videoUrl: string | null = searchParams.get("videoUrl");
+
+    if (!videoUrl) {
+        console.log("No video URL provided");
+        throw new Error("No video URL provided");
+    }
+
+    const transcription = await getVideoCaptions(videoUrl);
+
+    if (!transcription) {
+        console.log("No transcription found")
+        throw new Error("No transcription found");
+    }
+
+    // TODO: Make call to GPT
+    const spit = await processTranscriptionThroughAI(transcription);
+
+    if (!spit) {
+        throw new Error("No spit");
+    }
+
+    return json({ spit: spit });
+}
+
+async function processTranscriptionThroughAI(transcription: string): Promise<string | undefined> {
+    const configuration = new Configuration({
+        apiKey: OPENAI_API_KEY,
+    });
+
+    const openai = new OpenAIApi(configuration);
+
+    try {
+        const completion = await openai.createChatCompletion({
+            model: "gpt-4-0613",
+            messages: [
+                { "role": "system", "content": "The user will give you a transcription of a YouTube video. You should use this transcription to summarise the video into a TLDW (Too long, don't watch). You should use markdown to format the notes in a way that is efficient to read. Use bullet points when necessary." },
+                { role: "user", content: transcription }
+            ],
+        });
+
+        const responseString: string | undefined = completion.data.choices[0]?.message?.content;
+
+        return responseString;
+    } catch (error: any) {
+        console.log(error);
+        return error.toString();
+    }
+}
+
